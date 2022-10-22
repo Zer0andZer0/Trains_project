e3a61d11ea87b1d7cfba0ec9edb5f8ec8e7255f3
Vadim <yurvadim6@gmail.com>
2022-10-22 23:04:46 +0600

    update trains

--- trains/models.py
+++ trains/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from cities.models import City
@@ -10,7 +11,7 @@ class Train(models.Model):
     from_city = models.ForeignKey(City, on_delete=models.CASCADE,
                                   # null=True, blank=True,
                                   related_name='from_city_set',
-                                  verbose_name='Из какого города',
+                                  verbose_name='Из какого города ',
                                   )
     to_city = models.ForeignKey('cities.City', on_delete=models.CASCADE,
                                 related_name='to_city_set',
@@ -18,12 +19,26 @@ class Train(models.Model):
                                 )
 
     def __str__(self):
-        return f'Поезд №{self.name} из города{self.from_city}'
+        return f'Поезд №{self.name} из города {self.from_city}'
 
     class Meta:
         verbose_name = 'Поезд'
         verbose_name_plural = 'Поезда'
         ordering = ['travel_time']
+
+    def clean(self):
+        if self.from_city == self.to_city:
+            raise ValidationError('Изменить город прибытия')
+        qs = Train.objects.filter(
+            from_city=self.from_city, to_city=self.to_city,
+            travel_time=self.travel_time).exclude(pk=self.pk)
+        # Train == self.__class__
+        if qs.exists():
+            raise ValidationError('Изменить время в пути')
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
 
 
 class TrainTest(models.Model):
@@ -32,5 +47,5 @@ class TrainTest(models.Model):
     from_city = models.ForeignKey(City, on_delete=models.CASCADE,
                                   # null=True, blank=True,
                                   related_name='from_city',
-                                  verbose_name='Из какого города',
+                                  verbose_name='Из какого города ',
                                   )
